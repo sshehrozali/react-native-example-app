361d1207cb5245b1c5e6aadd4e2a7e1fe9c16976
Shehroz Ali <sshehroza13@gmail.com>
2022-03-21 21:59:54 +0500

    Uploaded files

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.main}>Wohooo! This is my First Mobile App! 😎</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1c1c21",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  main: {
+    color: "#fff",
+    fontSize: "25px",
   },
 });
